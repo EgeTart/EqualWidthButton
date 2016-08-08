0091f3a0907cb8419a6b784fcf5f47dc93d62e38
EgeTart <452051211@qq.com>
2016-08-08 13:47:36 +0800

    Equal Width Button
    Equal Width Button

---
+++ EqualWidthButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EqualWidthButton
+//
+//  Created by min-mac on 16/8/8.
+//  Copyright © 2016年 EgeTart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
